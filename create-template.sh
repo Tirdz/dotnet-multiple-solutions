6b355f48485e0066eaebc5724fc677f4032f35c0
Tirdz <141740903+Tirdz@users.noreply.github.com>
2023-08-20 18:42:38 +0800

    handle multiple spaces

--- create-template.sh
+++ create-template.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 cd ~/source/repos
 
-echo "Enter project name:"
-read projname
+read -p "Enter project name: " projname
+projname=$(echo "$projname" | tr -s ' ' ' ')
 
-echo "Creating solution for $projname"
-mkdir $projname
+echo "Creating solutions for $projname"
+mkdir "$projname"
+cd "$projname"
 
-cd $projname
+projname=$(echo "$projname" | tr -s ' ' '.')
 dotnet new sln -n $projname
 
-dotnet new mvc -o $projname.Web
+dotnet new mvc -o $projname.Api
 dotnet new classlib -o $projname.Application
 dotnet new classlib -o $projname.Domain
 dotnet new classlib -o $projname.Infrastructure
@@ -21,7 +22,7 @@ echo "Adding projects to solution"
 dotnet sln add **/*.csproj
 
 echo "Referencing projects"
-dotnet add $projname.Web reference $projname.Application/$projname.Application.csproj
+dotnet add $projname.Api reference $projname.Application/$projname.Application.csproj
 dotnet add $projname.Application reference $projname.Infrastructure/$projname.Infrastructure.csproj
 dotnet add $projname.Application reference $projname.External/$projname.External.csproj
 dotnet add $projname.Infrastructure reference $projname.Domain/$projname.Domain.csproj
